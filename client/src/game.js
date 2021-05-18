2161462cd1b6a7cbde5cc33b7fb387d45aa17492
Iain Moncrief <Iainmoncrief@gmail.com>
2021-05-18 13:33:24 -1000

    cleanup. fixed timing.

--- client/src/game.js
+++ client/src/game.js
@@ -15,97 +15,137 @@ function getRightVector(body) {
     };
 }
 
+// This is just a global variable for the game time.
+// This method is more efficient.
+export const BigBen = {
+    delta: 0.0,
+    start: null,
+    set deltaTime(delta) {
+        this.delta = delta/1000.0;
+    },
+    get deltaTime() {
+        return this.delta;
+    },
+    begin() {
+        this.start = new Date();
+    }
+};
+
+// Global manager for the key presses.
+export const Input = {
+    keys: [...new Array(256)].map(e => false), // Array of 256 false values.
+    get upArrow() { return Input.keys[38]; },
+    get rightArrow() { return Input.keys[39]; },
+    get leftArrow() { return Input.keys[37]; },
+    get downArrow() { return Input.keys[40]; }
+};
+
 export class Player {
+
+    /*
+        body: Matter.Body;
+    */
 
     constructor(spawnPos) {
         this.body = Bodies.trapezoid(spawnPos.x, spawnPos.y, 40, 40, 1);
-        this.keys = [];
     }
 
     updatePhysics() {
+        const dt = BigBen.deltaTime;
+        const body = this.body;
 
+        // Reset position
+        if (Input.downArrow) {
+            body.position = { x: 200, y: 200 };
+        }
+        // Jump
+        if (Input.upArrow) {
+            body.force = Vector.mult(getUpVector(body), 0.07 * dt);
+        }
+        // Spin left and right
+        if (Input.leftArrow && body.angularVelocity > -0.2) {
+            this.body.torque = -0.03 * dt;
+        } else {
+            if (Input.rightArrow && body.angularVelocity < 0.2) {
+                body.torque = 0.03 * dt;
+            }
+        }
     }
 
     update() {
-        this.movement();
-    }
 
-    movement() {
-        //reset position
-        if (this.keys[40]) {
-            this.body.position = { x: 200, y: 200 };
-        }
-        //jump
-        if (this.keys[38]) {
-            this.body.force = Vector.mult(getUpVector(this.body), 0.005);
-        }
-        //spin left and right
-        if (this.keys[37] && this.body.angularVelocity > -0.2) {
-            this.body.torque = -0.03;
-        } else {
-            if (this.keys[39] && this.body.angularVelocity < 0.2) {
-                this.body.torque = 0.03;
-            }
-        }
     }
 }
 
 
 export class Game {
 
-    constructor() {
-        // Game States
-        this.engine = Engine.create();
+    /*
+        engine: Matter.Engine;
+        runner: Matter.Runner;
 
-        this.box = Bodies.rectangle(450, 50, 80, 80);
-        this.ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
-        World.add(this.engine.world, [this.box, this.ground]);
-        this.players = [];
+        players: Player[];
+
+        box: Matter.Body;
+        ground: Matter.Ground;
+
+    */
+
+    constructor() {
+        this.engine = Engine.create();
         this.runner = Runner.create();
-        this.render = null;
+
+        this.players = [];
+        this.setup();
     }
 
+    // Setup the game controller.
     setup() {
+        // Sets up some sort of scene
+        this.box = Bodies.rectangle(450, 50, 80, 80);
+        this.ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+        // Adds the bodies into the world
+        World.add(this.engine.world, [this.box, this.ground]);
+
+        // Registers the update functions for each update.
         Events.on(this.engine, 'beforeUpdate', this.preUpdate.bind(this));
         Events.on(this.runner, 'tick', this.update.bind(this));
     }
 
-    load(data) {
-        this.engine.world = data;
-    }
-
-    onNewConnection() {
-        const newPlayer = new Player({ x: 200, y: 200 }, this.engine.world);
-        this.players.push(newPlayer);
-        return newPlayer;
-    }
-
+    // Adds a player into the game, as well as the players array.
     addPlayer(player) {
+        // Adds the player's physics body into the world.
         World.add(this.engine.world, player.body);
+
+        // Registers the player's functions to be called when an update happens.
         Events.on(this.engine, 'beforeUpdate', player.updatePhysics.bind(player));
         Events.on(this.runner, 'tick', player.update.bind(player));
+
+        // Adds the player to the array.
         this.players.push(player);
     }
 
-    onEndConnection(player) {
-        const index = this.players.indexOf(player);
-        if (index > -1) this.players.splice(index, 1);
-
-        World.remove(this.engine.world, player.body);
-    }
-
-    setRender(render) {
-        this.render = render;
-    }
-
+    // Called every time a new frame is rendered.
     update() {
-        
-    }
-    preUpdate() {
-
+        // Updates the global time variable.
+        BigBen.deltaTime = this.runner.delta;
     }
 
+    // Just called before anything else in the game, every frame.
+    preUpdate() {}
+
+    // Runs the game. This is not control blocking.
     run() {
+        // Starts Big Ben
+        BigBen.begin();
+        // Starts the Matter.js physics
         Runner.run(this.runner, this.engine);
     }
+
+    // Stops the game.
+    stop() {
+        Runner.stop(this.runner);
+    }
 }
+
--- client/src/main.js
+++ client/src/main.js
@@ -1,19 +1,17 @@
 import Thread from 'async-threading';
 import { Engine, Render, Runner, World, Events, Bodies, Body, Vector } from 'matter-js';
-import { Player, Game } from './game';
+import { Player, Game, Input } from './game';
 
+// Connects the Input manager to the DOM
+document.body.addEventListener('keydown', event => { Input.keys[event.keyCode] = true; });
+document.body.addEventListener('keyup', event => { Input.keys[event.keyCode] = false; });
+
+// Creates a new game and player
 const gameInstance = new Game();
-
-const keys = [...new Array(256)].map(e => false);
-document.body.addEventListener('keydown', event => { keys[event.keyCode] = true; });
-document.body.addEventListener('keyup', event => { keys[event.keyCode] = false; });
-
 const player = new Player({ x: 200, y: 200 });
-player.keys = keys;
 gameInstance.addPlayer(player);
 
-
-
+// Creates the renderer
 // https://github.com/liabru/matter-js/blob/master/src/render/Render.js#L66
 const render = Render.create({
     element: document.body,
@@ -29,12 +27,17 @@ const render = Render.create({
     }
 });
 
+// Centers renderer on the player before every update
 const cameraScale = 0.5;
 Events.on(render, 'beforeRender', () => Render.lookAt(render, player.body.position, {
     x: document.body.clientWidth * cameraScale,
     y: document.body.clientHeight * cameraScale
 }));
 
+// Starts the renderer
 Render.run(render);
 
+// Starts the game logic and physics
 gameInstance.run();
+
+setTimeout( () => gameInstance.stop(), 20000); // Stops game after 20 seconds bc laptop gets hot.
