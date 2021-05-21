0607ffef308fd96162a9b243bb0bfe516499cb13
Iain Moncrief <Iainmoncrief@gmail.com>
2021-05-20 21:10:58 -1000

    Worked on movement. However, things are starting to get messy. Might need to make a new folder.

--- client/src/game.js
+++ client/src/game.js
@@ -1,7 +1,9 @@
 import { Engine, Render, Runner, World, Events, Bodies, Body, Vector, SAT } from 'matter-js';
 
-const playerSpriteUrl = 'http://localhost:9000/sprites/player.png';
 
+function sprite(name, flipped = false) {
+    return 'http://localhost:9000/sprites/svg/' + name + (flipped ? '-flip' : '') + '.svg';
+}
 function getUpVector(body) {
     return {
         x: Math.cos(body.angle - (Math.PI / 2.0)),
@@ -67,14 +69,21 @@ export class Player {
         this.body = Bodies.trapezoid(spawnPos.x, spawnPos.y, 40, 40, 1, {
             render: {
                 sprite: {
-                    texture: playerSpriteUrl
+                    texture: sprite('angry-nohat', true),
+                    xScale: 1/3,
+                    yScale: 1/3,
                 }
-            }
+            },
+            friction: 0,
+            frictionStatic: 0,
         });
         this.body.label = 'gamer';
         this.isGrounded = false;
+        this.orientation = 1; // Bird looking right
 
         Body.setInertia(this.body, Infinity);
+        Body.setAngle(this.body, 0);
+        Body.scale(this.body, 1, 1);
 
     }
 
@@ -90,23 +99,25 @@ export class Player {
                 jump(body, 10);
             }
         }
-        // Spin left and right
+
         if (Input.leftArrow) {
-            // this.body.torque = -0.03 * dt;
-            // body.force = Vector.mult(getRightVector(body), -0.07 * dt * (1.0/safeMag(body.velocity)));
-            horizontalMovement(body, -3.0 * dt);
-            body.friction = 0;
-            body.frictionStatic = 0;
+            if (this.orientation > 0) this.flip();
         } else if (Input.rightArrow) {
-            // body.torque = 0.03 * dt;
-            // body.force = Vector.mult(getRightVector(body), 0.07 * dt * (1.0/safeMag(body.velocity)));
-            horizontalMovement(body, 3.0 * dt);
-            body.friction = 0;
-            body.frictionStatic = 0;
-        } else {
-            body.friction = 1;
-            body.frictionStatic = 1;
+            if (this.orientation < 0) this.flip();
         }
+
+        if (Input.leftArrow || Input.rightArrow) {
+            horizontalMovement(body, 3.0 * dt * this.orientation);
+        } else if (this.isGrounded) {
+            Body.setVelocity(body, { x: 0, y: 0 });
+        }
+    }
+
+    flip() {
+        this.orientation *= -1;
+        Body.setAngle(this.body, 0);
+        Body.scale(this.body, -1, 1);
+        this.body.render.sprite.texture = sprite('angry-nohat', this.orientation > 0);
     }
 
     update() {
@@ -176,7 +187,7 @@ export class Game {
         this.box = Bodies.rectangle(450, 50, 80, 80);
         this.ground = Bodies.rectangle(400, 610, 10000, 60, { isStatic: true });
         this.ground.label = 'ground';
-        this.friction = 1;
+        this.friction = 0;
         this.frictionStatic = 0;
 
         // Adds the bodies into the world
