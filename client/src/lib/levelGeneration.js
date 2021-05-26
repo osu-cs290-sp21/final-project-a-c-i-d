19cb704fb1efa2e2847412c7ee026a2d69b53c84
korotkyd <korotkyd@oregonstate.edu>
2021-05-26 03:39:54 -0700

    Change jump key to spacebar Increase maximum platform separation

--- client/src/lib/levelGeneration.js
+++ client/src/lib/levelGeneration.js
@@ -1,7 +1,5 @@
 import { Bodies, Body } from 'matter-js';
 
-// export const vector = ([x,y]) => ({ x: x, y: y });
-
 const platforms = ['dirt-platform'];
 
 const platformOptions = {
@@ -11,11 +9,11 @@ const platformOptions = {
     restitution: 1
 };
 
-export const makePlatform = ([x, y], [width, height]) => Bodies.rectangle(x, y, width, height, platformOptions);
+export const makePlatform = ([x, y], [width, height]) => Bodies.rectangle(x, y, width, height, platformOptions)
 
-export const range = (a,b,n) => [... new Array(n).keys()].map(x => a + ((b-a) * x / n));
+export const range = (a,b,n) => [... new Array(n).keys()].map(x => a + ((b-a) * x / n))
 
-export const generateTerrain = ([startX, startY], amount) => range(0,500 * amount, amount)
+export const generateTerrain = ([startX, startY], amount) => range(0,800 * amount, amount)
         .map(x => [x, Math.sin(x * 20) * 150 ]) // (x,f(x))
-        .map(([x,y]) => makePlatform([startX - x,startY - y], [200 * Math.random(Math.random(50)) + 50,20]))
-        .map(platform => [platform, Body.setStatic(platform, true)].shift());
+        .map(([x,y]) => makePlatform([startX - y, startY - x], [200 * Math.random(Math.random(50)) + 50,20]))
+        .map(platform => [platform, Body.setStatic(platform, true)].shift())
--- client/src/lib/stateControllers.js
+++ client/src/lib/stateControllers.js
@@ -19,10 +19,10 @@ export const BigBen = {
 // Global manager for the key presses.
 export const Input = {
     keys: [...new Array(256)].map(e => false), // Array of 256 false values.
-    get upArrow() { return Input.keys[87]; },
+    get upArrow()    { return Input.keys[82]; },
     get rightArrow() { return Input.keys[68]; },
-    get leftArrow() { return Input.keys[65]; },
-    get downArrow() { return Input.keys[83]; }
+    get leftArrow()  { return Input.keys[65]; },
+    get downArrow()  { return Input.keys[83]; }
 };
 
 export const GameStats = {
