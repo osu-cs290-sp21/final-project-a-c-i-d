7de9fdce04a17b99edceadeb4d7dbbcde15ddeec
andy <atongsak@gmail.com>
2021-05-26 09:35:02 -0700

    Cleaned up server main.js

--- server/main.js
+++ server/main.js
@@ -1,47 +1,41 @@
 /**
- * Server side tasks: 
+ * Server side tasks:
  * - Leaderboard
  * - Avatar selection
  * - Maybe dynamically serve the rules content?
  */
 
 /* Variables and constants */
-const db = require('./db');
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
-const exphbs = require('express-handlebars');
+const db = require("./db");
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const exphbs = require("express-handlebars");
 const app = express();
 
 var portnumber = process.env.PORT || 9000;
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, "/../client/dist")));
 
-/* Creates a table of (name, score) values */
-/* Add (photo, name, score) later */
+// Add live leaderboard server update
 const hashmap = {};
-app.route('/postScore', (req,res) => {
+app.route("/leaderboard", (req, res) => {
   hashmap[req.params.name] = req.params.score;
 })
 
-//async function main() {
+// Use handlebars to serve leaderboard
 
-    app.use(express.static(path.join(__dirname, '/../client/dist')));
+// Add avatar choice update
 
+// GET / - this would normally get the home page
+app.get("/", (req, res, next) => {
+  res.send("Hello from the express server!");
+});
 
-
-    // GET / - this would normally get the home page
-    app.get('/', (req, res, next) => {
-      res.send("Hello from the express server!")
-    })
-
-    // Start the server
-    app.listen(portnumber, () => {
-      console.log("== Server running on port " + portnumber)
-    })
-
-//}
-
-//main();
+// Start the server
+app.listen(portnumber, () => {
+  console.log("== Server running on port " + portnumber);
+});
