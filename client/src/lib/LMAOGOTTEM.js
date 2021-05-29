92b76e04f4ec334d7ebf3fd4457f74adc6886a70
Iain Moncrief <Iainmoncrief@gmail.com>
2021-05-28 14:17:48 -1000

    Cut out a lot of unneded code

--- client/src/lib/LMAOGOTTEM.js
+++ client/src/lib/LMAOGOTTEM.js
@@ -1,17 +1,9 @@
 
 
 export const awakenRickAstley = () => {
-    const source = (src,type) => {
-        const sourceElement = document.createElement('source');
-        sourceElement.setAttribute('src', src);
-        sourceElement.setAttribute('type', type);
-        return sourceElement;
-    }
-    // const video = document.createElement('video').appendChild(source('img/finale.webm', 'video/webm'));
-    // const audio = document.createElement('audio').appendChild(source('sounds/finale.webm', 'audio/webm'));
-    const video = document.getElementById('finale-video');
-    video.classList.remove('gone');
-    video.classList.add('fill-display');
-    const audio = document.getElementById('finale-audio');
-   // [video, audio].map(e => e.play());
+    // const video = document.getElementById('finale-video');
+    // video.classList.remove('gone');
+    // video.classList.add('fill-display');
+    // const audio = document.getElementById('finale-audio');
+    // [video, audio].map(e => e.play());
 }
