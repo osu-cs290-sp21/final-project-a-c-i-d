829182eb95a635e800be0010c6a6f31f0b950025
andy <andy.st2001@gmail.com>
2021-06-04 21:42:24 -0700

    hmm. i tried with the leaderboard.

--- server/server.js
+++ server/server.js
@@ -1,16 +1,9 @@
 const express = require('express')
-const { fstat } = require('fs')
+const fs = require('fs')
 const path = require('path')
 const app = express()
 const port = 3000
-
-const json = JSON.parse(getJsonFile())
-const jsonAsArray = Object.keys(json).map(function (key) {
-    return json[key];
-})
-.sort(function (itemA, itemB) {
-    return itemA.score < itemB.score
-})
+var dataFile = JSON.parse(fs.readFileSync('leaderboardData.json'))
 
 const dontGame = true;
 // app.get('/build.js', (req,res,next) => {
@@ -42,12 +35,12 @@ app.put('/died', (req, res) => {
 // not sure if this works yet...
 app.post('/leaderboard', function (req, res, next) {
     console.log("== req.body:", req.body)
-    if (req.body && req.body.username && req.body.score) {
+    if (req.body && req.body.username && req.body.altitude) {
 
         if (leaderboardData) {
             leaderboardData.push({ 
                 username: req.body.username,
-                score: req.body.score
+                score: req.body.altitude
             })
             console.log("== leaderboardData:", leaderboardData)
             fs.writeFile(
@@ -73,4 +66,18 @@ app.listen(port, () => {
     console.log(`listen to ${port}`)
 })
 
+/**
+ * Sort JSON file by particular key
+ * Note: Key is the score/altitude of the birdie once they die
+ * @param {int} key JSON key to use
+ * @param {Object}
+ */ 
+ function sortBy(key, data) {
+    return data.sort((a, b) => {
+        var x = parseInt(a[key]);
+        var y = parseInt(b[key]);
+        return ((x > y) ? -1 : ((x < y) ? 1 : 0))
+    })
+}
+var sortedData = sortBy('score', dataFile)
 
