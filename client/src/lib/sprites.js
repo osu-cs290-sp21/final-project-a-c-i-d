f79e03f769ba59dc85629c7054f06e5d7ada1bb9
Iain Moncrief <Iainmoncrief@gmail.com>
2021-06-08 16:08:42 -1000

    working on birdy pngs

--- client/src/lib/sprites.js
+++ client/src/lib/sprites.js
@@ -9,7 +9,8 @@ const root = origin;
 export const birdNames = ['bella', 'harry', 'olive', 'perry', 'sahana', 'todd'];
 export const ogBirds = ['andy-bluebird', 'david-penguin', 'cole-kakapo', 'iain-shamathrush'];
 
-export const sprite = (name, flipped = false) => asset(['img', 'sprites', 'svg', name + (flipped ? '-flip' : '') + '.svg'].join('/'));
+// export const sprite = (name, flipped = false) => asset(['img', 'sprites', 'svg', name + (flipped ? '-flip' : '') + '.svg'].join('/'));
+export const sprite = (name, flipped = false) => asset(['img', 'sprites', 'png', name + (flipped ? '-flip' : '') + '.svg'].join('/'));
 export const birdAssetNames = [...ogBirds, ...birdNames.map(name => name + '-day')];
 const choose = arr => arr[Math.floor(Math.random() * arr.length)];
 export const randomBird = () => choose(birdAssetNames);
