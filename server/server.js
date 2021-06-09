d6970fe4f7bf47b6ef738116e30787cc7afe3e3e
Iain Moncrief <Iainmoncrief@gmail.com>
2021-06-09 01:34:56 -1000

    heroku push

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const path = require('path')
 const fs = require('fs')
 
 const app = express()
-const port = process.env.PORT ? process.env.PORT : 3000
+const port = (process.env.PORT || 5000)
 if (!fs.existsSync('leaderboardData.json')) {
     fs.writeFileSync('leaderboardData.json', '[]')
 }
